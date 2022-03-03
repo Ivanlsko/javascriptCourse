18629a9212dcadfac1febc17c521373a34c2af32
Ivan Lesko <65163471+Ivanlsko@users.noreply.github.com>
2022-03-03 10:16:10 +0100

    LECTURE: Data Types

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -1,9 +1,17 @@
 /* 1. Declarevariablescalled'country','continent'and'population'and assign their values according to your own country (population in millions)
 2. Logtheirvaluestotheconsole */
 
-/* let country = "Slovakia";
+let country = "Slovakia";
 let continent = "Europe";
 let population = 5500000;
-console.log(country + " " + continent + " " + population); */
+console.log(country + " " + continent + " " + population);
 
 ///////////
+
+/* 1. Declareavariablecalled'isIsland'andsetitsvalueaccordingtoyour country. The variable should hold a Boolean value. Also declare a variable 'language', but don't assign it any value yet
+2. Logthetypesof'isIsland','population','country'and'language' to the console */
+
+let isIsland = false;
+let language;
+
+console.log(typeof isIsland + " " + population + " " + country + " " + language);
