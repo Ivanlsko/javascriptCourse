007b47135083459dfac6b80d09072e9f56f0d791
Ivan Lesko <65163471+Ivanlsko@users.noreply.github.com>
2022-03-03 10:26:58 +0100

    LECTURE: let, const and var

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -1,8 +1,8 @@
 /* 1. Declarevariablescalled'country','continent'and'population'and assign their values according to your own country (population in millions)
 2. Logtheirvaluestotheconsole */
 
-let country = "Slovakia";
-let continent = "Europe";
+const country = "Slovakia";
+const continent = "Europe";
 let population = 5500000;
 console.log(country + " " + continent + " " + population);
 
@@ -11,7 +11,7 @@ console.log(country + " " + continent + " " + population);
 /* 1. Declareavariablecalled'isIsland'andsetitsvalueaccordingtoyour country. The variable should hold a Boolean value. Also declare a variable 'language', but don't assign it any value yet
 2. Logthetypesof'isIsland','population','country'and'language' to the console */
 
-let isIsland = false;
-let language;
+const isIsland = false;
+const language = "Slovak";
 
 console.log(typeof isIsland + " " + population + " " + country + " " + language);
