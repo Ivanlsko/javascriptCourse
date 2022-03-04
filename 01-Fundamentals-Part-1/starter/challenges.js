9a0503b52d94e855d781272b6e48fcb47846d67f
Ivan Lesko <65163471+Ivanlsko@users.noreply.github.com>
2022-03-04 18:32:23 +0100

    Coding Challenge #1

---
+++ 01-Fundamentals-Part-1/starter/challenges.js
@@ -0,0 +1,17 @@
+//<------------------------------ Coding Challenge #1 ------------------------------ >//
+
+let marksMass = 78;
+let marksHeight = 1.69;
+let marksBMI = marksMass / marksHeight ** 2;
+console.log(marksBMI);
+
+let johnsMass = 92;
+let johnsHeight = 1.95;
+let johnsBMI = johnsMass / johnsHeight ** 2;
+console.log(johnsBMI);
+
+//There is no need of using if statements
+const markHigherBMI = marksBMI > johnsBMI;
+console.log(markHigherBMI);
+
+//<------------------------------ Coding Challenge #2 ------------------------------ >//
